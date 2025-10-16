b7c7398fbbefc354982d1fa27109aae7a0f68ef6
unyuicho-creator <unyui.cho.1999@gmail.com>
2025-10-17 00:21:13 +0900

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -3,7 +3,4 @@ variable "region" {
   default = "ap-northeast-1"
 }
 
-variable "site_dir" {
-  type        = string
-  description = "Path to the local site directory that contains index.html"
-}
+
